12045c4a5334924a6103b546455a46c6398bb97a
Kevin <kevin.ramirezcorr@campusucc.edu.co>
2025-10-07 14:37:27 +0000

    mejoras y uso de geters y seters

--- Biclioteca.java
+++ Biclioteca.java
@@ -5,12 +5,12 @@ public class Biclioteca {
         System.out.println(" --- Anadiendo ’Cien Anos de Soledad ’ ---");
         Libro libro1 = new Libro(" Cien Anos de Soledad ", " Gabriel Garcia Marquez ", 1967, 496);
         // Uso del toString () y un getter
-        System.out.println(" Datos iniciales : " + libro1 ) ;
-        System.out.println(" Autor : " + libro1.getAutor() ) ;
+        System.out.println("Datos iniciales : " + libro1 ) ;
+        System.out.println("Autor : " + libro1.getAutor()) ;
 
         // Uso de un setter y un metodo de negocio
         libro1.setAnioPublicacion(1970) ;
-        libro1.prestar () ;
+        libro1.prestar() ;
 
         // Verificar estado con toString ()
         System.out.println("Estado final: " + libro1);
@@ -19,5 +19,10 @@ public class Biclioteca {
         System.out.println("\n--- Anadiendo ’El Senor de los Anillos ’ ---") ;
         Libro libro2 = new Libro("El Senor de los Anillos ", "J.R.R. Tolkien ", 1954, 1216) ;
         libro2.prestar();
+        System.out.println(libro2.getNumeroPaginas());
+        System.out.println(libro2.getAutor());
+        System.out.println(libro2.getTitulo());
+        libro2.setAnioPublicacion(1990);
+        System.out.println(libro2.getAnioPublicacion());
     }
 }
